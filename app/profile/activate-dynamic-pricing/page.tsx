a088f0a268fcc29eef8b40f54ead32243fcf112f
Vladislav Bykanov <vbykanov1989@outlook.com>
2023-10-06 10:34:11 +0200

    add loader when waiting for DP

--- app/profile/activate-dynamic-pricing/page.tsx
+++ app/profile/activate-dynamic-pricing/page.tsx
@@ -51,6 +51,7 @@ export default function ActivateDynamicPricing() {
 	};
 
 	const handleContinue = async () => {
+		setIsLoading(true);
 		const { data } = await axios.post('/api/dynamic-pricing/getPrice', {
 			location: user.location.country.toLowerCase(),
 			category: user.serviceCategory,
@@ -60,80 +61,87 @@ export default function ActivateDynamicPricing() {
 
 		const recomendedPrice = data.suggestion;
 
-		if (!recomendedPrice)
+		if (!recomendedPrice) {
+			setIsLoading(false);
 			return setDynamicPricingError(
 				'Server might be slow in responding. Please try again.'
 			);
-		else {
+		} else {
+			setIsLoading(false);
 			setDynamicPrice(recomendedPrice);
 			setConfirmPage(true);
 		}
 	};
 	return (
 		<main className='setDynamicPricePage'>
-			{!confirmPage && (
-				<div className='setMinPriceWindow'>
-					<div>
-						<h2>Set Dynamic</h2>
-						<h2>Pricing Rules</h2>
-					</div>
-					<p className='text'>
-						What is the least amount you are willing to earn for 30
-						min call?
-					</p>
+			{!confirmPage &&
+				(isLoading ? (
+					<Loader />
+				) : (
+					<div className='setMinPriceWindow'>
+						<div>
+							<h2>Set Dynamic</h2>
+							<h2>Pricing Rules</h2>
+						</div>
+						<p className='text'>
+							What is the least amount you are willing to earn for
+							30 min call?
+						</p>
 
-					<div className='setMinPriceBox'>
-						<label htmlFor='minPriceInput'>
-							<b>$</b>
-							<input
-								type='text'
-								placeholder='00.00'
-								value={minPrice == 0 ? '' : minPrice}
-								onChange={(e) => {
-									const result = e.target.value.replace(
-										/[^0-9]/gi,
-										''
-									);
+						<div className='setMinPriceBox'>
+							<label htmlFor='minPriceInput'>
+								<b>$</b>
+								<input
+									type='text'
+									placeholder='00.00'
+									value={minPrice == 0 ? '' : minPrice}
+									onChange={(e) => {
+										const result = e.target.value.replace(
+											/[^0-9]/gi,
+											''
+										);
 
-									setMinPrice(Number(result));
+										setMinPrice(Number(result));
+									}}
+								/>
+							</label>
+							<button
+								disabled={minPrice === 0}
+								style={{
+									borderWidth: 0,
+									backgroundColor:
+										minPrice === 0 ? '#00000040' : 'black',
+									cursor:
+										minPrice === 0
+											? 'not-allowed'
+											: 'pointer',
+									color: 'white',
 								}}
-							/>
-						</label>
-						<button
-							disabled={minPrice === 0}
+								onClick={handleContinue}
+							>
+								Continue
+							</button>
+							<button
+								onClick={() => router.back()}
+								style={{ cursor: 'pointer' }}
+							>
+								Cancel
+							</button>
+						</div>
+						<div
 							style={{
-								borderWidth: 0,
-								backgroundColor:
-									minPrice === 0 ? '#00000040' : 'black',
-								cursor:
-									minPrice === 0 ? 'not-allowed' : 'pointer',
-								color: 'white',
+								display: 'flex',
+								alignItems: 'center',
+								height: 40,
+								color: 'rgb(255, 74, 74)',
+								fontWeight: '500',
+								textAlign: 'center',
 							}}
-							onClick={handleContinue}
 						>
-							Continue
-						</button>
-						<button
-							onClick={() => router.back()}
-							style={{ cursor: 'pointer' }}
-						>
-							Cancel
-						</button>
+							{dynamicPricingError}
+						</div>
 					</div>
-					<div
-						style={{
-							display: 'flex',
-							alignItems: 'center',
-							height: 40,
-							color: 'rgb(255, 74, 74)',
-							fontWeight: '500',
-							textAlign: 'center',
-						}}
-					>
-						{dynamicPricingError}
-					</div>
-				</div>
-			)}
+				))}
 			{confirmPage &&
 				(isLoading ? (
 					<Loader />
